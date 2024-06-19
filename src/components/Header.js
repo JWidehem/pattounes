457df6d167b87700ef236c02e99c3bf7f8d42e15
Widehem Jimmy <widehem.jimmy@gmail.com>
2024-06-20 01:59:53 +0200

    Add Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.scss'; // Assurez-vous de lier le fichier SCSS pour les styles
+import logo from '../img/logo-color.png'; // Importez le logo modifié
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+      <nav className="nav">
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
           <li><Link to="/gallery">Gallery</Link></li>
           <li><Link to="/booking">Booking</Link></li>
-          <li><Link to="/admin">Admin Dashboard</Link></li>
-          <li><Link to="/login">Login</Link></li>
         </ul>
       </nav>
     </header>
