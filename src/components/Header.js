709c9548f7962e50299247e1c98a212ff6e8d074
Widehem Jimmy <widehem.jimmy@gmail.com>
2024-06-20 02:36:55 +0200

    Add Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.scss'; // Assurez-vous de lier le fichier SCSS pour les styles
 import logo from '../img/logo-color.png'; // Importez le logo modifié
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="header">
-      <div className="logo-container">
-        <Link to="/">
-          <img src={logo} alt="Logo" className="logo" />
-        </Link>
-      </div>
       <nav className="nav">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/booking">Booking</Link></li>
+          <li>
+            <Link
+              to="/services"
+              className={location.pathname === '/services' ? 'active' : ''}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={location.pathname === '/about' ? 'active' : ''}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </Link>
+        </div>
+        <ul>
+          <li>
+            <Link
+              to="/gallery"
+              className={location.pathname === '/gallery' ? 'active' : ''}
+            >
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/booking"
+              className={location.pathname === '/booking' ? 'active' : ''}
+            >
+              Booking
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
